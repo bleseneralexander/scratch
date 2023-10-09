2da09d49d0a6055c2e0e06f972504a9d2d207537
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2023-10-08 19:26:13 -0500

    Update typ.sql

--- typ.sql
+++ typ.sql
@@ -2,3 +2,15 @@ Create User Rob
 Rob == /py.py/"string"
 insert USER in 'myTimeClock'
 insert User in "scr.json" as "py.py/index()"
+item VAR in User as "scr.json/PAY"
+create VAR In user in column
+for VAR in column
+where VAR = User in 'myTimeClock'
+VAR -= NULL
+END;
+create VAR in User in Row
+for VAR in Row
+where VAR = PAY in "scr.json/PAY"
+VAR -= VAR
+end;
+
