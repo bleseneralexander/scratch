152d4148513d9b57a1e60fbd29fdd7ea28baf3c7
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2023-10-10 16:50:28 -0500

    Update gin.js

--- gin.js
+++ gin.js
@@ -5,9 +5,9 @@ url in urllib.request
 urlopen www.targetWorkday.com
 var two == innerhtml(urlopen)
 two = 'myTimeClock'
-two.scriptVar in "scr.json"
- two.scriptVar -= VAR in "scr.son"
+two.scriptVar in "/scr.json"
+ two.scriptVar -= VAR in "/scr.son"
 scriptVar.string = "Robin M"/.-
   scriptVar.string.two == deleteVar
-delete two.scriptVar.string in "scr.json"
-deleteVar -= VAR in scr.sql
+delete two.scriptVar.string in "/scr.json"
+deleteVar -= VAR in "/typ.sql"
